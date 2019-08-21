4557068daf28b9dfd6772f94a66ebc741a3cd6dd
DAEHYUNKIM <zcvfcat@gmail.com>
2019-08-21 20:49:26 +0900

    t

--- src/container/bbs/ReadPaneContainer.js
+++ src/container/bbs/ReadPaneContainer.js
@@ -5,8 +5,7 @@ import GeneralActionButton from "component/bbs/read/GeneralActionButton";
 import GeneralSubTitle from "component/bbs/general/GeneralSubTitle";
 import { readAction, delAction } from "store/module/bbs/defaultbbs";
 
-const ReadPaneContainer = (props) => {
-
+const ReadPaneContainer = (props) => {  
   useEffect(()=>{
     if(props.action === "del"){
       alert("글이 삭제 되었습니다.");
@@ -26,19 +25,6 @@ const ReadPaneContainer = (props) => {
       name: props.bbs.name,
       pw: pw
     })
-    // await AjaxBbs.del(props.idx, {
-    //   idx: props.idx,
-    //   name: props.bbs.name,
-    //   pw: pw
-    // }).then(({ data }) => {
-    //   console.log(data);
-    //   if (data.message === Message.success) {
-    //     alert("성공");
-    //     props.history.push("/");
-    //   } else if (data.message === Message.fail) {
-    //     alert("비밀번호를 다시 넣어주세요!");
-    //   }
-    // });
   }
 
   return (
@@ -77,117 +63,3 @@ export default connect(
     }
   })
 )(ReadPaneContainer);
-
-// class ReadPaneContainer extends Component {
-//   state = {
-//     res: {}
-//   };
-
-//   board = {};
-//   idx = "";
-
-//   async componentDidMount() {
-//     const res = await AjaxBbs.read(this.props.pageNum);
-//     this.board = res.data;
-//     this.idx = this.board.idx;
-//     this.setState({
-//       res: res
-//     });
-//   }
-
-//   renderBoard = ({ res } = this.state, { board } = this) => {
-//     if (res.status === 200) {
-//       let vBoard = [];
-//       for (const key in board) {
-//         vBoard.push(
-//           <tr key={key}>
-//             <td>{key}</td>
-//             <td>
-//               {key === "idx" ? (
-//                 <input name={key} value={board[key]} readOnly={true} />
-//               ) : key === "name" ? (
-//                 <input name={key} value={board[key]} readOnly={true} />
-//               ) : key === "pw" ? (
-//                 <input name={key} />
-//               ) : key === "content" ? (
-//                 <textarea name={key} value={board[key]} readOnly={true}>
-//                   {board[key]}
-//                 </textarea>
-//               ) : (
-//                 <input value={board[key]} readOnly={true} />
-//               )}
-//             </td>
-//           </tr>
-//         );
-//       }
-//       vBoard.push(
-//         <tr>
-//           <td>
-//             <Link to={UrlBbs.update + board["idx"]}>
-//               <button>업데이트</button>
-//             </Link>
-//           </td>
-//         </tr>
-//       );
-//       return vBoard;
-//     }
-//   };
-
-//   onSubmit = (e) => {
-//     e.preventDefault();
-//     let data = {};
-//     const formData = new FormData(e.target);
-//     for (let key of formData.keys()) {
-//       data[key] = formData.get(key);
-//     }
-
-//     AjaxBbs.del(data)
-//       .then(({ data }) => {
-
-//         if (data.message === Message.success) {
-//           alert("성공");
-//           this.props.history.push("/");
-//         } else if (data.message === Message.fail) {
-//           alert("비밀번호를 다시 넣어주세요!");
-//         }
-//       })
-//       .catch((e) => {
-//         console.log(e);
-//       });
-//   };
-
-//   onDel = (e)=>{
-//     e.preventDefault();
-//     let pw = prompt("비밀번호를 입력하세요");
-//     AjaxBbs.del(this.idx,{
-//       idx:this.idx,
-//       name:this.board.name,
-//       pw:pw,
-//     }).then(({data})=>{
-//       console.log(data);
-//       if (data.message === Message.success) {
-//         alert("성공");
-//         this.props.history.push("/");
-//       } else if (data.message === Message.fail) {
-//         alert("비밀번호를 다시 넣어주세요!");
-//       }
-//     })
-//   }
-
-//   render() {
-//     console.log(
-//       this.action,
-//       this.board,
-//       this.idx,
-
-//     );
-
-//     return (
-//       <div>
-//         <GeneralSubTitle subtitle={"글"}></GeneralSubTitle>
-//         <GeneralReadData content={this.board} />
-//         <GeneralActionButton idx={this.idx} history={this.props.history} del={this.onDel}/>
-//       </div>
-//     );
-//   }
-// }
