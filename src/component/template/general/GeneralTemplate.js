15414bdf1d0313affdeb0960ae35c75c31d5cba9
YoungWon94 <chmp1408@gmail.com>
2019-08-27 12:59:25 +0900

    1

--- src/component/template/general/GeneralTemplate.js
+++ src/component/template/general/GeneralTemplate.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CommonHeader from "component/common/header";
+
 import CommonFooter from "component/common/footer";
 const GeneralTemplate = (props) => {
   let childrens = [];
@@ -10,9 +11,16 @@ const GeneralTemplate = (props) => {
     <div>
       {childrens["top"] ? childrens["top"] : <CommonHeader />}
       <div>
+        <aside>
+          {childrens["left"] ? childrens["left"] : ''}
+        </aside>
         <article>
           {childrens["section"]}
+
         </article>
+        <aside>
+          {childrens["right"] ? childrens["right"] : ''}
+        </aside>
       </div>
       {childrens["bottom"] ? childrens["bottom"] : <CommonFooter />}
     </div>
